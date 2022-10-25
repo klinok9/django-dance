957bf5983fa6e371addce591cb931b5180855b84
klinok9 <1.4.hoffer@gmail.com>
2022-10-25 19:07:24 +0300

    Динамический URL

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,17 +2,19 @@ from django.urls import path
 from . import views  #from . импорт из этой папки (horoscope)
 
 urlpatterns = [
-    path('Aries/', views.Aries),  # 'horoscope/leo', представление которое в файлике views
-    path('Taurus/', views.Taurus),
-    path('Gemini/', views.Gemini),
-    path('Cancer/', views.Cancer),
-    path('Leo/', views.Leo),
-    path('Virgo/', views.Virgo),
-    path('Libra/', views.Libra),
-    path('Scorpio/', views.Scorpio),
-    path('Sagittarius/', views.Sagittarius),
-    path('Capricorn/', views.Capricorn),
-    path('Aquarius/', views.Aquarius),
-    path('Pisces/', views.Pisces),
+
+    path('<sign_zodiak>/', views.info_about_sign)  # <sign_zodiak>-роут как параметр
+    # path('Aries/', views.Aries),  # 'horoscope/leo', представление которое в файлике views
+    # path('Taurus/', views.Taurus),
+    # path('Gemini/', views.Gemini),
+    # path('Cancer/', views.Cancer),
+    # path('Leo/', views.Leo),
+    # path('Virgo/', views.Virgo),
+    # path('Libra/', views.Libra),
+    # path('Scorpio/', views.Scorpio),
+    # path('Sagittarius/', views.Sagittarius),
+    # path('Capricorn/', views.Capricorn),
+    # path('Aquarius/', views.Aquarius),
+    # path('Pisces/', views.Pisces),
 
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,50 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseNotFound
 
 
 # Create your views here.
-def Aries(reuest):
-    return HttpResponse("Знак зодиака Aries")
+# def Aries(reuest):
+#     return HttpResponse("Знак зодиака Aries")
+#
+#
+# def Taurus(request):
+#     return HttpResponse("знак зодиака Taurus")
+#
+#
+# def Gemini(request):
+#     return HttpResponse("знак зодиака Gemini")
+#
+#
+# def Cancer(request):
+#     return HttpResponse("знак зодиака Cancer")
+#
+#
+# def Leo(request):
+#     return HttpResponse("знак зодиака Leo")
+#
+#
+# def Virgo(request):
+#     return HttpResponse("знак зодиака Virgo")
+#
+#
+# def Libra(request):
+#     return HttpResponse("знак зодиака Libra")
+#
+#
+# def Scorpio(request):
+#     return HttpResponse("знак зодиака Libra")
+#
+#
+# def Sagittarius(request):
+#     return HttpResponse("знак зодиака Sagittarius")
+#
+#
+# def Capricorn(request):
+#     return HttpResponse("знак зодиака Capricorn")
+#
+#
+# def Aquarius(request):
+#     return HttpResponse("знак зодиака Aquarius")
+#
+# def Pisces(request):
+#     return HttpResponse("знак зодиака Pisces")
 
-
-def Taurus(request):
-    return HttpResponse("знак зодиака Taurus")
-
-
-def Gemini(request):
-    return HttpResponse("знак зодиака Gemini")
-
-
-def Cancer(request):
-    return HttpResponse("знак зодиака Cancer")
-
-
-def Leo(request):
-    return HttpResponse("знак зодиака Leo")
-
-
-def Virgo(request):
-    return HttpResponse("знак зодиака Virgo")
-
-
-def Libra(request):
-    return HttpResponse("знак зодиака Libra")
-
-
-def Scorpio(request):
-    return HttpResponse("знак зодиака Libra")
-
-
-def Sagittarius(request):
-    return HttpResponse("знак зодиака Sagittarius")
-
-
-def Capricorn(request):
-    return HttpResponse("знак зодиака Capricorn")
-
-
-def Aquarius(request):
-    return HttpResponse("знак зодиака Aquarius")
-
-def Pisces(request):
-    return HttpResponse("знак зодиака Pisces")
+# Динамический Url
+def info_about_sign (requst, sign_zodiak):
+    if sign_zodiak == 'leo':
+        return HttpResponse("знак зодиака leo")
+    elif sign_zodiak == 'Aries':
+        return HttpResponse("Знак зодиака Aries")
+    elif sign_zodiak == 'Taurus':
+        return HttpResponse("Знак зодиака Taurus")
+    else:
+        return HttpResponseNotFound(f'{sign_zodiak} - ne znak')
